ced8aebf45ec87fb3c3c16fb8987033d45817d17
Joshwlks <66643537+Joshwlks@users.noreply.github.com>
2020-06-26 00:00:44 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,9 +19,9 @@ function setup() {
   
   
  
- var cw = displayWidth / 4;
- var pts1 = font.textToPoints('PragmatiQ', cw, 300, 192);
- var pts2 = font.textToPoints('Solutions', cw, 475, 175);
+ //var cw = displayWidth / 4;
+ var pts1 = font.textToPoints('PragmatiQ', 0, 300, 192);
+ var pts2 = font.textToPoints('Solutions', 0, 475, 175);
   //console.log(pts1);
   //console.log(pts2);
   
