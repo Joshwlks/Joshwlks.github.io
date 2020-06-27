80c134d4757a78a733b64a2047dc00f9a123951a
Joshwlks <66643537+Joshwlks@users.noreply.github.com>
2020-06-27 20:44:56 +0100

    Update sketch.js

--- Code1/sketch.js
+++ Code1/sketch.js
@@ -1,7 +1,4 @@
 var font;
-var txt1;
-var x1;
-var y1;
 var vehicles1 = [];
 var vehicles2 = [];
   
@@ -19,13 +16,7 @@ function setup() {
   noStroke();
   textAlign(CENTER,TOP);
   text('Welcome to joshwilkins.co.uk', 683, 50);
-  //textSize(192);
-  //textAlign(CENTER);
-  txt1 = 'Projects'; //text(, 683, 200);
-  x = width/2 - 300;
-  y = height/4;
-  var pts1 = font.textToPoints(txt1, x, y, 192);
-	
+  
   textSize(100);
   textAlign(RIGHT);
   text('Blog', 320, 500);
@@ -33,17 +24,10 @@ function setup() {
   textSize(100);
   textAlign(LEFT);
   text('Other', 1024.5, 500);
-}
 
-  
-  
- 
+var pts1 = font.textToPoints('Projects', width/2, height/2, 192);
+var pts2 = font.textToPoints('Solutions', 400, 475, 175);
 
-
- //var pts1 = font.textToPoints(txt3);
- //var pts2; //font.textToPoints('Solutions', 400, 475, 175);
-  //console.log(pts1);
-  //console.log(pts2);
   
 	  for (var i = 0; i < pts1.length; i++){
 		var pt = pts1[i];
@@ -54,14 +38,14 @@ function setup() {
 		//point(pt.x, pt.y);
 		}
 
-	  /*for (var i = 0; i < pts2.length; i++){
+	  for (var i = 0; i < pts2.length; i++){
 		var pt = pts2[i];
 		var vehicle = new Vehicle(pt.x, pt.y);
 		vehicles2.push(vehicle);
 		//stroke(255);
 		//strokeWeight(8);
 		//point(pt.x, pt.y);
-		} */
+		} 
 
 }  
 
@@ -79,12 +63,12 @@ function draw() {
 		v1.show();
 		}
 
-	  /*for(var i = 0; i < vehicles2.length; i++){
+	  	for(var i = 0; i < vehicles2.length; i++){
 		var v2 = vehicles2[i];
 		v2.behaviours();
 		v2.update();
 		v2.show();
-		} */
+		} 
 	
 } 
 
