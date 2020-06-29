f77730672f6e279a24cbac8036f140062a44a0f5
Joshwlks <66643537+Joshwlks@users.noreply.github.com>
2020-06-29 03:03:56 +0100

    Update sketch.js

--- Code1/sketch.js
+++ Code1/sketch.js
@@ -6,27 +6,27 @@ function preload() {
   font = loadFont('ChunkFive-Regular.otf'); 
 } 
    
-//  Canvas area and color  
-function setup() { 
-  createCanvas(displayWidth, displayHeight); 
-  background(51);
-  textFont(font)
-  textSize(75); 
-  fill(255); 
-  noStroke();
-  textAlign(CENTER,TOP);
-  text('Welcome to joshwilkins.co.uk', 683, 50);
-  
-  textSize(100);
-  textAlign(RIGHT);
-  text('Blog', 320, 500);
-	
-  textSize(100);
-  textAlign(LEFT);
-  text('Other', 1024.5, 500);
+//  Canvas area and color
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
 
-var pts1 = font.textToPoints('Projects', width/2-100, height/2, 192);
-var pts2 = font.textToPoints('Solutions', width/2 - 200, height/2, 192);
+function setup() { 
+  var wW = windowWidth; 
+  var wH = windowHeight;
+  cnv = createCanvas(wW, wH); 
+  cnv.style('display', 'block');
+  textFont(font);
+  
+  textSize(250);
+  var p ='Coming Soon';
+  var pW = textWidth(p);
+  
+
+var pts1 = font.textToPoints('Coming Soon', wW/2 - pW/2, wH/2, 250);
+//var pts2 = font.textToPoints('Solutions', width/2 - 200, height/2, 192);
 
   
 	  for (var i = 0; i < pts1.length; i++){
@@ -38,14 +38,14 @@ var pts2 = font.textToPoints('Solutions', width/2 - 200, height/2, 192);
 		//point(pt.x, pt.y);
 		}
 
-	  for (var i = 0; i < pts2.length; i++){
+	  /*for (var i = 0; i < pts2.length; i++){
 		var pt = pts2[i];
 		var vehicle = new Vehicle(pt.x, pt.y);
 		vehicles2.push(vehicle);
 		//stroke(255);
 		//strokeWeight(8);
 		//point(pt.x, pt.y);
-		} 
+		} */
 
 }  
 
@@ -63,12 +63,12 @@ function draw() {
 		v1.show();
 		}
 
-	  	for(var i = 0; i < vehicles2.length; i++){
+	  	/*for(var i = 0; i < vehicles2.length; i++){
 		var v2 = vehicles2[i];
 		v2.behaviours();
 		v2.update();
 		v2.show();
-		} 
+		} */
 	
 } 
 
